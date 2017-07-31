ec2fee04d178faa23e5264fca1612bd1ac1ac7e4
zhjm <943257156@qq.com>
2017-07-31 15:03:05 +0800

    1

--- result.php
+++ result.php
@@ -7,8 +7,6 @@ while ($row = mysqli_fetch_array($result)) {
 	$user = array('id' => $row["id"], 'user' => $row["user"], 'password' => $row["password"], 'level' => $row["level"], 'time' => $row["time"]);
 	$data[] = $user;
 	
-	
-	
 	/*echo $row["id"].' '.$row["user"].' '.$row["password"].' '.$row["level"].' '.$row["time"];
 	echo "<br/>";*/
 }
