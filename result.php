66e131910532b1539550bfedc5df1dba2f3dd8b6
zhjm <943257156@qq.com>
2017-07-31 15:02:41 +0800

    1

--- result.php
+++ result.php
@@ -4,14 +4,17 @@ include 'conn.php';
 $result = mysqli_query($conn, "SELECT * FROM USER");
 
 while ($row = mysqli_fetch_array($result)) {
-	/*$user = array('id' => $row["id"], 'user' => $row["user"], 'password' => $row["password"], 'level' => $row["level"], 'time' => $row["time"]);
-	$data[] = $user;*/
-	echo $row["id"].' '.$row["user"].' '.$row["password"].' '.$row["level"].' '.$row["time"];
-	echo "<br/>";
+	$user = array('id' => $row["id"], 'user' => $row["user"], 'password' => $row["password"], 'level' => $row["level"], 'time' => $row["time"]);
+	$data[] = $user;
+	
+	
+	
+	/*echo $row["id"].' '.$row["user"].' '.$row["password"].' '.$row["level"].' '.$row["time"];
+	echo "<br/>";*/
 }
 
-/*$json = json_encode($data);
-echo "{" . '"user"' . ":" . $json . "}";*/
+$json = json_encode($data);
+echo "{" . '"user"' . ":" . $json . "}";
 
 mysqli_close($conn);
 ?>
