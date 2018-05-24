52cc0e59662708196c38d7f521614064a1bc171f
zhjm <943257156@qq.com>
2018-05-24 10:58:16 +0800

    Update conn.php

--- php/conn.php
+++ php/conn.php
@@ -1,8 +1,8 @@
 <?php
-$dbhost = "148.66.136.137";
-$username = "zhjm";
-$userpass = "ZHJM520gmj620";
-$dbdatabase = "H5game";
+$dbhost = "";
+$username = "";
+$userpass = "";
+$dbdatabase = "";
 //链接数据库
 $conn = mysqli_connect($dbhost, $username, $userpass, $dbdatabase);
 if (mysqli_connect_error()) {
